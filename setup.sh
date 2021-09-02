5a74e958d5744e64588fbc667ed4c386d4fe0693
Renan Martins <58703271+heynan0@users.noreply.github.com>
2021-09-02 13:30:56 -0400

    Add aliases and update a few things

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-alias gf="git fetch"
-alias gs="git status"
-
-git config --global alias.br branch
-git config --global alias.st status
+cat aliases >> ~/.bashrc
+cat gitconfig >> ~/.gitconfig
+# git config --global alias.br branch
+# git config --global alias.st status
