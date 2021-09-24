c91978ec868700c47a1704041a881ded651de0b5
Renan Martins <58703271+heynan0@users.noreply.github.com>
2021-09-24 10:05:59 -0400

    Fix coa_function.sh not being sourced

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 cat aliases >> ~/.bashrc
-echo "source coa_function.sh" >> ~/.bashrc 
+cat coa_function.sh >> ~/.bashrc
 cat gitconfig >> ~/.gitconfig
 # git config --global alias.br branch
 # git config --global alias.st status
