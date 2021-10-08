54b84917cf8f4ee4e9aa44aedbf1e6d0a149058e
Renan Martins <58703271+heynan0@users.noreply.github.com>
2021-10-08 16:32:11 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,4 @@
 cat aliases >> ~/.bashrc
 cat coa_function.sh >> ~/.bashrc
 cat gitconfig >> ~/.gitconfig
-echo "extension:"
-code --install-extension eamodio.gitlens
-
-echo "echo installextinbashrc; code --install-extension zxh404.vscode-proto3" >> ~/.bashrc
+cat install_vscode_extensions >> ~/.bashrc
